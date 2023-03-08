8acb37da5a8f4f7b37ccfa32e7055f28af4c6ca5
Julys-esp <125158160+Julys-esp@users.noreply.github.com>
2023-03-08 12:56:46 -0600

    Add files via upload

---
+++ act5,6,7.js
@@ -0,0 +1,16 @@
+//Convierte a mayusculas la frase ‘al que madruga Dios lo ayuda’.
+//Convierte a mayusculas la frase ‘al que madruga Dios lo ayuda’.
+let frase="al que madruga Dios lo ayuda";
+console.log(frase.toUpperCase());
+
+//Convierte a minusculas la frase“MAS VALE PAJARO EN MANO QUE CIENTOS VOLANDO”
+let frase2="MAS VALE PAJARO EN MANO QUE CIENTOS VOLANDO";
+console.log(frase2.toLowerCase());
+
+/*Se tiene las variablesa.const saludo = ‘HoLa’;
+b.const saludo2 = ‘mUnDo’;
+Concatena las variables y realiza las transformaciones necesarias para Imprimir en pantalla ‘hola MUNDO’*/
+
+const saludo="HoLa";
+const saludo2="mUnDo";
+console.log(saludo.toLowerCase() +" "+ saludo2.toUpperCase() );
