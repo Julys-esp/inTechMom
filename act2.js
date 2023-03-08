2ce532f5e940fdb42fa861b3c35297c523ed18dd
Julys-esp <125158160+Julys-esp@users.noreply.github.com>
2023-03-08 12:25:42 -0600

    Add files via upload

---
+++ act2.js
@@ -0,0 +1,17 @@
+/*Creaunavariablellamadacityyasigna5lugaresqueaparezcanenlapelículaoseríe. 
+Después de cada asignación imprime el contenido de la variable.*/
+let city;
+city="Ciudad Satan";
+console.log(city);
+
+city="Palacio Kamisama";
+console.log(city);
+
+city="Namekusei";
+console.log(city);
+
+city="Camino de la serpiente";
+console.log(city);
+
+city="Kame House";
+console.log(city);
