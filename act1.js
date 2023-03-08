fe8019474e981280b233d7f1ecc7f6cbd7871fdd
Julys-esp <125158160+Julys-esp@users.noreply.github.com>
2023-03-08 12:01:47 -0600

    Add files via upload

---
+++ act1.js
@@ -0,0 +1,8 @@
+//Creatres(3)variablesparaasignarelnombre,apellidoyedaddelpersonajeprincipal de una película o serie que te guste
+
+let nombre="Goku";
+let apellido="Kakarotto";
+let edad=54;
+console.log("Nombre: "+ nombre + " "+"Tipo: "+typeof nombre);
+console.log("Apellido: "+ apellido + " "+"Tipo: "+typeof apellido);
+console.log("Edad: "+ edad + " "+"Tipo: "+typeof edad);
