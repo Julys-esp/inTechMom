f9260edac5d141e619e7d409e2a98dcfea408c44
Julissa Espinosa <julissa.espinosa.cabrales@gmail.com>
2023-03-21 12:00:56 -0600

    act4

---
+++ act4.js
@@ -0,0 +1,44 @@
+/* Completa las predicciones! hay un listado con tipos de datos,
+primero escribe en la predicción qué dato crees que es y luego ejecuta el comando para verificar si tu respuesta
+ es correcta
+*/
+
+console.log("Prediccion: Number")
+console.log("Actual: "+typeof(20));
+
+console.log("Prediccion: boolean")
+console.log("Actual: "+typeof(true));
+
+console.log("Prediccion: Number")
+console.log("Actual: "+typeof("1"+"3"));
+
+console.log("Prediccion: String")
+console.log("Actual: "+typeof("hamburgers" - "s"));
+
+console.log("Prediccion: Number")
+console.log("Actual: "+typeof(95.5));
+
+console.log("Prediccion: Number")
+console.log("Actual: "+typeof(1 != 2));
+
+console.log("Prediccion: String")
+console.log("Actual: "+typeof("johnny" + 5));
+
+console.log("Prediccion: number")
+console.log("Actual: "+typeof(NaN));
+
+console.log("Prediccion: String")
+console.log("Actual: "+typeof("1" - "3"));
+
+console.log("Prediccion: number")
+console.log("Actual: "+typeof("johnny" - 5));
+
+console.log("Prediccion: String")
+console.log("Actual: "+typeof("Hello"));
+
+console.log("Prediccion: String")
+console.log("Actual: "+typeof("hamburger" + "s"));
+
+console.log("Prediccion: string")
+console.log("Actual: "+typeof(99 * "luftbaloons"));
+
